07f146857d38c33c78be5d063ec5e3d5aa64e2df
Ryo Tsukamoto <tuka.rbao@gmail.com>
2020-05-10 23:11:05 +0900

    add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'home#top'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  resources :users, only: [:index, :show]
 end
