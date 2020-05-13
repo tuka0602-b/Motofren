ae9301a483e492ddacdf03571c3726edf4dcae69
Ryo Tsukamoto <tuka.rbao@gmail.com>
2020-05-13 17:00:21 +0900

    ユーザー編集をパスワード入力なしで行えるように編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,17 @@ class User < ApplicationRecord
     user.name = auth.info.name
     user
   end
+
+  # deviseのupdate_without_passwordを参考に
+  def update_without_current_password(params)
+    params.delete(:current_password)
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update(params)
+    clean_up_passwords
+    result
+  end
 end
