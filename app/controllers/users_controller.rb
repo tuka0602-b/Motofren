250698b43d579d69f3db05b78be50e19c8943635
Ryo Tsukamoto <tuka.rbao@gmail.com>
2020-05-17 16:53:32 +0900

    image_posts controllerを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @image_posts = @user.image_posts.all.recent
   end
 
   def index
---
+++ spec/requests/image_posts_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "ImagePosts", type: :request do
+
+end
