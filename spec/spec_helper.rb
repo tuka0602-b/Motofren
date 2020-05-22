8828bdadf9c31cf090fc958702bd405b816c4271
Ryo Tsukamoto <tuka.rbao@gmail.com>
2020-05-22 11:33:35 +0900

    chromedriver-helper gem を webdrivers gemに変更

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,7 @@ require 'capybara/rspec'
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :selenium_chrome_headless
+    driven_by :selenium_chrome_headless, screen_size: [1400, 1400]
   end
 
   # rspec-expectations config goes here. You can use an alternate
