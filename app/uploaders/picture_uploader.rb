ff154e7a38ab9acfdb41e640b735b7198cb6256d
Ryo Tsukamoto <tuka.rbao@gmail.com>
2020-05-25 23:51:46 +0900

    carrire_waveをコメントアウト

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,11 +5,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [700, 700]
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
     storage :file
-  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
