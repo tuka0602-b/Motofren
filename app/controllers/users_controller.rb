1a3e80afa989faf10cb6a63eca7e9d4fc7adc561
Ryo Tsukamoto <tuka.rbao@gmail.com>
2020-06-01 12:17:40 +0900

    フォロー、フォロワーページを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,18 @@ class UsersController < ApplicationController
     @image_posts = @user.image_posts.recent.page(params[:page]).per(12)
     @image_post = current_user.image_posts.build if user_signed_in?
   end
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.page(params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.page(params[:page])
+    render 'show_follow'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-  resources :users, only: [:show] do
+  resources :users, only: :show do
     member do
       get :following, :followers
     end
   end
   resources :image_posts, only: [:show, :create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
