6d877c8758b6660329d3de143d32a0d87591bb0f
Ryo Tsukamoto <tuka.rbao@gmail.com>
2020-06-09 15:04:13 +0900

    talk_rooms/showページを修正, スタイル追加

--- app/controllers/talk_rooms_controller.rb
+++ app/controllers/talk_rooms_controller.rb
@@ -1,4 +1,6 @@
 class TalkRoomsController < ApplicationController
   def show
+    @talk_room = TalkRoom.find(params[:id])
+    @messages = @talk_room.messages
   end
 end
--- app/models/talk_room.rb
+++ app/models/talk_room.rb
@@ -1,5 +1,6 @@
 class TalkRoom < ApplicationRecord
   belongs_to :recruitment
+  has_many :messages
   validates :recruitment_id, presence: true
   validates :name, presence: true, length: { maximum: 50 }
 end
