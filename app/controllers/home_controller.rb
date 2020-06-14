ec867eeb70fc265c5fb566443bc6b74800f55470
Ryo Tsukamoto <tuka.rbao@gmail.com>
2020-06-14 17:49:18 +0900

    ransackで募集一覧の検索機能実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @q = Recruitment.ransack(params[:q])
-    @recruitments = @q.result.includes([:user, :talk_room]).recent
+    @recruitments = @q.result.includes([:user, :area, :talk_room]).recent
   end
 end
