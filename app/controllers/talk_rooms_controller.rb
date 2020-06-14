c611ed698148b80fc017dd9e17ff9f954039a49d
Ryo Tsukamoto <tuka.rbao@gmail.com>
2020-06-14 22:57:20 +0900

    ransackでトークルームのユーザーー検索を実装

--- app/controllers/talk_rooms_controller.rb
+++ app/controllers/talk_rooms_controller.rb
@@ -1,6 +1,11 @@
 class TalkRoomsController < ApplicationController
   def show
-    @talk_room = TalkRoom.includes(messages: [:user]).find(params[:id])
+    @talk_room = TalkRoom.find(params[:id])
+    @q = @talk_room.users.ransack(params[:q])
+    @users = @q.result(distinct: true)
+    @messages = Message.where(
+      "user_id IN (?) AND talk_room_id = ?", @users.ids, @talk_room.id
+    ).includes(:user)
     @message = current_user.messages.build
   end
 end
--- app/models/talk_room.rb
+++ app/models/talk_room.rb
@@ -1,6 +1,7 @@
 class TalkRoom < ApplicationRecord
   belongs_to :recruitment
   has_many :messages, dependent: :destroy
+  has_many :users, through: :messages
   validates :recruitment_id, presence: true
   validates :name, presence: true, length: { maximum: 55 }
 end
