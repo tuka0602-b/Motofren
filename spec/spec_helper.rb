b7d78f1164bc71d37cd1895abc057504dc602ebc
Ryo Tsukamoto <tuka.rbao@gmail.com>
2020-06-21 15:34:05 +0900

    footer追加、header.footer固定、headless_chromeのスクリーンサイズ調整

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,7 @@ require 'capybara/rspec'
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :selenium_chrome_headless, screen_size: [1400, 1400]
+    driven_by :selenium, using: :headless_chrome, screen_size: [800, 1000]
   end
 
   # rspec-expectations config goes here. You can use an alternate
